5b790e12a8850d5cbd1dbb90bb2fd11eff7b61c8
Hai <lhhai.1208@gmail.com>
2022-06-24 23:42:14 +0700

    modified blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,10 +1,32 @@
 import "./Blog.css";
-import ss1 from "../../assets/img/students/ss1.png";
-import ss2 from "../../assets/img/students/ss2.jpg";
-import ss3 from "../../assets/img/students/ss3.jpg";
-import ss4 from "../../assets/img/students/ss4.jpg";
+import Posts from "./Posts";
 
 const Blog = () => {
+  const blog = (
+    <div className="row row-bottom-padded-md">
+      {Posts.map((post) => (
+        <div className="col-lg-4 col-md-4 col-sm-6">
+          <div class="blog animate-box">
+            <a href="#">
+              <img class="img-responsive" src={post.img} alt="" />
+            </a>
+            <div class="blog-text">
+              <div class="prod-title">
+                <h3>
+                  <a href="">{post.title}</a>
+                </h3>
+                <span class="posted_by">{post.date}</span>
+                <p>{post.content.slice(0, 120) + "..."}</p>
+                <p>
+                  <a href="#">Learn More...</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
   return (
     <div id="blog-section">
       <div class="container">
@@ -14,103 +36,7 @@ const Blog = () => {
           </div>
         </div>
       </div>
-      <div class="container">
-        <div class="row row-bottom-padded-md">
-          <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="blog animate-box">
-              <a href="#">
-                <img class="img-responsive" src={ss1} alt="" />
-              </a>
-              <div class="blog-text">
-                <div class="prod-title">
-                  <h3>
-                    <a href="">Story from Latifa, form five student</a>
-                  </h3>
-                  <span class="posted_by">Dec 4, 2019</span>
-                  <p>
-                    My name is Latifa Hassani Sadick, a form five student at
-                    Nsimbo Secondary School. I come from a poor family, ...
-                  </p>
-                  <p>
-                    <a href="#">Learn More...</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="blog animate-box">
-              <a href="#">
-                <img class="img-responsive" src={ss2} alt="" />
-              </a>
-              <div class="blog-text">
-                <div class="prod-title">
-                  <h3>
-                    <a href="">Story from Jenipher, form one student</a>
-                  </h3>
-                  <span class="posted_by">Aug 20, 2019</span>
-                  <p>
-                    My name is Jenipher Johanes Magidanga, I was born on 20th
-                    July 2009. I am a form one student at Tushikamane Secondary
-                    School ...
-                  </p>
-                  <p>
-                    <a href="#">Learn More...</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="blog animate-box">
-              <a href="#">
-                <img class="img-responsive" src={ss3} alt="" />
-              </a>
-              <div class="blog-text">
-                <div class="prod-title">
-                  <h3>
-                    <a href="">Story from Saada , form five student</a>
-                  </h3>
-                  <span class="posted_by">Aug 20, 2019</span>
-                  <p>
-                    My name is Saada Seif, a form five student at Mji mkuu
-                    Secondary School. I am an orphan whom was abandoned by my
-                    relatives ...
-                  </p>
-                  <p>
-                    <a href="#">Learn More...</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-              <div class="blog animate-box">
-                <a href="#">
-                  <img class="img-responsive" src={ss4} alt="" />
-                </a>
-                <div class="blog-text">
-                  <div class="prod-title">
-                    <h3>
-                      <a href="">Story from William, form four student</a>
-                    </h3>
-                    <span class="posted_by">Aug 20, 2019</span>
-                    <p>
-                      My name is William Frederick, a student at Tushikamane
-                      Secondary School, I graduated my lower secondary education
-                      this year (2019) ...
-                    </p>
-                    <p>
-                      <a href="#">Learn More...</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div class="container">{blog}</div>
     </div>
   );
 };
---
+++ src/components/Blog/Posts.js
@@ -0,0 +1,41 @@
+import ss1 from "../../assets/img/students/ss1.png";
+import ss2 from "../../assets/img/students/ss2.jpg";
+import ss3 from "../../assets/img/students/ss3.jpg";
+import ss4 from "../../assets/img/students/ss4.jpg";
+
+const Posts = [
+  {
+    id: "ss1",
+    title: "Story from Latifa, form five student",
+    date: "Dec 4, 2019",
+    img: ss1,
+    content:
+      "My name is Latifa Hassani Sadick, a form five student at Nsimbo Secondary School. I come from a poor family, both my parents are not financially capable to support my education. My parents are charcoal retail sellers, the money they earn from the business sometimes fail to support us with food. Thus, my education has been supported by people who are touched by my family situation and my academic progress in school. Because, I have been among the best top 5 students in my class. In 2017, I got a terrible accident that involved a locomotive and school bus. The accident led me to ICU, and got operation on my backbone. Now I am not 100% okay. Because, I have to attend clinic every month at Muhimbili National Hospital. I decided to share my story with FIEN Foundation, so as I can get people who are interested to support my education and improvement of my health. I need school uniform, health insurance, stationery, school fee, transport fare, pocket money, and textbooks.",
+  },
+  {
+    id: "ss2",
+    title: "Story from Jenipher, form one student",
+    date: "Aug 20, 2019",
+    img: ss2,
+    content:
+      "My name is Jenipher Johanes Magidanga, I was born on 20th July 2009. I am a form one student at Tushikamane Secondary School. On 6th July 2016 my father passed away, when I was still in standard 5. My life changed to worse! Because, my life was depending everything on my father. My mother had no job, and thus she could not help. When it came to our senses that my father is no longer with us, my mother started manual work. She has been washing clothes and making cleanliness in the houses located in our neighborhood. The work does not give her good money, thus sometimes we fail to eat our dinner, and with empty stomach I have to go to school. My school life is terrible, I do not have reliable support for my education. I do not see my future, sometime I do not come to school! Because, my mother could not afford to provide me with transport fare, or buying sanitation towels during my menstruation period. The school do not have adequate text books, therefore I do not have learning materials to study on my own. I decided to approach FIEN Foundation and share my story for any Samaritan to support my education. I need  school uniform, transport fare, health insurance, food and textbooks. ",
+  },
+  {
+    id: "ss3",
+    title: "Story from Saada , form five student",
+    date: "Aug 20, 2019",
+    img: ss3,
+    content:
+      "My name is Saada Seif,  a form five student at Mji mkuu Secondary School. I am an orphan  whom was abandoned by my relatives after the death of parents. I  decided to work as a house girl; cleaning, cooking, and carrying out manual labour in one of the houses in town. Doing  domestic works led me to earn  little money to support my education. But it was difficult to balance work and school responsibilities. Thus my academic performance  suffered  though I was one of the best students in my class.Without support from good Samaritans, I could  not be able  to finish my ordinary level secondary education at Tushikamane Secondary School.  I thank God that I was  able to complete  my ordinary secondary education and perform well with merit grade. I was selected to join advanced secondary education at Mji Mkuu Secondary School.   But, I do not have any financial support to cover my schooling  cost.  I need  funds for school uniform, health insurance, stationary, school fee, transport, pocket money, and textbooks.",
+  },
+  {
+    id: "ss4",
+    title: "Story from William, form four student",
+    date: "Aug 20, 2019",
+    img: ss4,
+    content:
+      "My name is William Frederick, a student at Tushikamane Secondary School, I graduated my  lower secondary education this year (2019) at Tushikamane Secondary School. I come from a poor family, my dad passed on when I was still in primary school, at that time my  mother  was sick of kidney failure. My mother became ill when she was pregnant  of my young brother. After the death of my father  things become more worse, my family  was not able even to get a tea for break fast, since my mum was still sick. Thus,  I had to stop going to school and turned into a beggar so as I can  support my family. I started attending school less frequently and began feeling depressed.  I sometimes arrived at school with an empty stomach. I also had to walk a significant distance from  my home to  to school. Despite these hurdles, I thank God that I have been one of the top 10 students  in my class of 170.  I approached FIEN Foundation, so as I can share my story for any Samaritan to support my Advanced Secondary Education in  the year 2020-2022. I  need of  financial support for school uniform,  health insurance,  stationery,  school fee,  transport,  pocket money, and  textbooks.",
+  },
+];
+
+export default Posts;
